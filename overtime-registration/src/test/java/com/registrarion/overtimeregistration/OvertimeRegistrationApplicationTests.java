ea0e1d6400c0c44c9b08f1538a12a79425cc92c6
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-03-06 20:59:27 +0800

    setup spring

---
+++ overtime-registration/src/test/java/com/registrarion/overtimeregistration/OvertimeRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registrarion.overtimeregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvertimeRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
