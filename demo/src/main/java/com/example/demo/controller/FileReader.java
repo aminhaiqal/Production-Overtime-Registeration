6963b7b900ad285053af51f05bad303383f36f41
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-03-08 11:35:00 +0800

    updated

--- demo/src/main/java/com/example/demo/controller/FileReader.java
+++ demo/src/main/java/com/example/demo/controller/FileReader.java
@@ -34,7 +34,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 public class FileReader {
     @PostMapping("/upload")
     public String readExcelFile(@RequestParam("file") MultipartFile file) throws IOException {
-        System.out.println("Starting to read file...");
+        int startRow = 3;
         // Read the Excel file using Apache POI
         Workbook workbook = WorkbookFactory.create(file.getInputStream());
 
@@ -42,8 +42,11 @@ public class FileReader {
         Sheet sheet = workbook.getSheetAt(0);
 
         StringBuilder sb = new StringBuilder();
+        // Get the starting row
+        Row firstRow = sheet.getRow(startRow);
+        
         // Loop through the rows in the sheet
-        for (Row row : sheet) {
+        for (Row row = firstRow; row != null;) {
             // Loop through the cells in the row
             for (Cell cell : row) {
                 switch (cell.getCellType()) {
@@ -67,7 +70,12 @@ public class FileReader {
                         sb.append("\t");
                 }
             }
-            sb.append("\n");
+            //sb.append("\n");
+            String[] result = splitStringBuilder(sb, "\t");
+            //assignString(result);
+
+            System.out.println("\n");
+            break;
         }
         // Close the workbook using try-with-resources
         try (workbook) {}
@@ -78,4 +86,35 @@ public class FileReader {
         return "";
     }
 
+    private String[] splitStringBuilder(StringBuilder sb, String delimiter) {
+        String[] result = sb.toString().split(delimiter);
+        return result;
+    }
+
+    public void assignString (String[] result) {
+        int staff_id = Integer.parseInt(result[0]);
+        String name = result[1];
+        String dept = result[2];
+        String section = result[3];
+        Boolean MON = Boolean.parseBoolean(result[4]);
+        Boolean TUE = Boolean.parseBoolean(result[5]);
+        Boolean WED = Boolean.parseBoolean(result[6]);
+        Boolean THU = Boolean.parseBoolean(result[7]);
+        Boolean FRI = Boolean.parseBoolean(result[8]);
+        Boolean SAT = Boolean.parseBoolean(result[9]);
+        Boolean SUN = Boolean.parseBoolean(result[10]);
+
+        System.out.println(staff_id);
+        System.out.println(name);
+        System.out.println(dept);
+        System.out.println(section);
+        System.out.println(MON);
+        System.out.println(TUE);
+        System.out.println(WED);
+        System.out.println(THU);
+        System.out.println(FRI);
+        System.out.println(SAT);
+        System.out.println(SUN);
+
+    }
 }
